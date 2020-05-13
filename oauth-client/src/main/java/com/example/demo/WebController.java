1b9a8b8b4391797f51736a3fdfdd92f94c41cf92
Peter Lau <peterl@hk1.ibm.com>
2020-05-13 20:22:15 +0800

    initial

---
+++ oauth-client/src/main/java/com/example/demo/WebController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+    @RequestMapping("/securedPage")
+    public String securedPage(Model model, Principal principal) {
+        return "securedPage";
+    }
+    @RequestMapping("/")
+    public String index(Model model, Principal principal) {
+        return "index";
+    }
+}
