6767b3ee500656837cbf3ea1329efd9009472095
Peter Lau <peterl@hk1.ibm.com>
2020-05-15 21:18:33 +0800

    react native redirect

--- ReactNativeAuthClient/App.js
+++ ReactNativeAuthClient/App.js
@@ -1,126 +1,102 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
+  Button,
   View,
+  SafeAreaView,
   Text,
-  StatusBar,
+  Alert,
+  Page,
+  Form,
+  FormLabel,
+  FormValue,
 } from 'react-native';
+import {authorize} from 'react-native-app-auth';
+import {revoke} from 'react-native-app-auth';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-// import { authorize } from 'react-native-app-auth';
-
-// const config = {
-//   issuer: '<YOUR_ISSUER_URL>',
-//   clientId: '<YOUR_CLIENT_ID>',
-//   redirectUrl: '<YOUR_REDIRECT_URL>',
-//   scopes: ['<YOUR_SCOPES_ARRAY>'],
-// };
-
-// const result = await authorize(config);
-
-
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+const config = {
+  // issuer: 'https://dev-326028.okta.com/oauth2/default',
+  clientId: 'R2dpxQ3vPrtfgF72',
+  redirectUrl: 'com.auth:/callback',
+  scopes: ['user_info'],
+  serviceConfiguration: {
+    authorizationEndpoint: 'https://10.0.2.2:9081/auth/oauth/authorize',
+    tokenEndpoint: 'https://10.0.2.2:9081/auth/oauth/token',
+    // revocationEndpoint: 'https://demo.identityserver.io/connect/revoke',
+  },
+  usePKCE: false,
+  // clientSecret: 'fDw7Mpkk5czHNuSRtmhGmAGL42CaxQB9',
+  // additionalParameters:{
+  //   response_type: 'code',
+  // }
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+class App extends React.Component {
+  state = {
+    accessToken: '',
+    accessTokenExpirationDate: '',
+  };
+
+  handleAuthorize = async () => {
+    try {
+      console.log("start");
+      const newAuthState = await authorize(config);
+      console.log("end");
+      console.log(newAuthState);
+
+      this.setState({
+        accessToken: newAuthState.accessToken,
+        accessTokenExpirationDate: newAuthState.accessTokenExpirationDate,
+        refreshToken: newAuthState.refreshToken,
+      });
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Failed to log in', error.message);
+    }
+  };
+
+  handleLogout = async () => {
+    try {
+      console.log(this.state.refreshToken);
+      const result = await revoke(config, {
+        tokenToRevoke: this.state.accessToken,
+        sendClientId: true,
+      });
+      console.log(result);
+
+      this.setState({
+        accessToken: '',
+        accessTokenExpirationDate: '',
+      });
+    } catch (error) {
+      Alert.alert('Failed to log in', error.message);
+    }
+  };
+
+  render() {
+    return (
+      <View>
+        <Button
+          onPress={this.handleAuthorize}
+          title="Auth"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <Button
+          onPress={this.handleLogout}
+          title="Logout"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <View>
+          <Text>accessToken</Text>
+          <Text>{this.state.accessToken}</Text>
+          <Text>accessTokenExpirationDate</Text>
+          <Text>{this.state.accessTokenExpirationDate}</Text>
+        </View>
+      </View>
+    );
+  }
+}
 
 export default App;
